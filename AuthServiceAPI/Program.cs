4a6fe0473abddb922a8e869730afc46fac7cd265
rasmussig <rasmus2001@live.dk>
2024-12-13 11:30:28 +0100

    Refactor Program.cs: streamline configuration setup, enhance logging, and improve error handling for environment variables

--- AuthServiceAPI/Program.cs
+++ AuthServiceAPI/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using VaultSharp;
 using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
 using VaultSharp.V1.Commons;
 using NLog;
 using NLog.Web;
@@ -11,7 +12,7 @@ using Microsoft.Extensions.Logging;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
-using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication; 
 using AuthServiceAPI.Services;
 using AuthServiceAPI.Models;
 using AuthServiceAPI.Interfaces;
@@ -22,72 +23,37 @@ logger.Debug("init main");
 
 try
 {
-    // Initial setup: Configuration, Logger, and Vault
     var builder = WebApplication.CreateBuilder(args);
     var configuration = builder.Configuration;
 
-    // Register Guid serializer for MongoDB
-    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String)); // Konfigurerer MongoDB til at gemme GUID'er som strings.
+    BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
 
-    // Initialize Vault to retrieve secrets
     var vaultService = new VaultService(configuration);
+
     string mySecret = await vaultService.GetSecretAsync("secrets", "SecretKey") ?? "????";
     string myIssuer = await vaultService.GetSecretAsync("secrets", "IssuerKey") ?? "????";
     string myConnectionString = await vaultService.GetSecretAsync("secrets", "MongoConnectionString") ?? "????";
-
-    // Add retrieved secrets to application configuration
+    
     configuration["SecretKey"] = mySecret;
     configuration["IssuerKey"] = myIssuer;
     configuration["MongoConnectionString"] = myConnectionString;
 
-    // Log secret details for debugging purposes
     Console.WriteLine("Issuer: " + myIssuer);
     Console.WriteLine("Secret: " + mySecret);
     Console.WriteLine("MongoConnectionString: " + myConnectionString);
 
-    // Validate MongoDB connection string
-    if (string.IsNullOrEmpty(myConnectionString))
+    string connectionString = myConnectionString;
+    if (string.IsNullOrEmpty(connectionString))
     {
-        logger.Error("ConnectionString not found in environment variables");
+        logger.Error("ConnectionString not found in environment vaariables");
         throw new Exception("ConnectionString not found in environment variables");
     }
     else
     {
-        logger.Info($"ConnectionString: {myConnectionString}");
+        logger.Info("ConnectionString: {0}", connectionString);
     }
 
-    // Dependency Injection: Register services
-    builder.Services.AddTransient<VaultService>(); // Vault service
-    builder.Services.AddControllers(); // Controllers for API endpoints
-    builder.Services.AddEndpointsApiExplorer(); // Endpoint Explorer for Swagger
-    builder.Services.AddSwaggerGen(); // Swagger for API documentation
-
-    // Configure HttpClient for UserService
-    var userServiceUrl = Environment.GetEnvironmentVariable("userservicehost");
-    if (string.IsNullOrEmpty(userServiceUrl))
-    {
-        logger.Error("UserServiceUrl not found in environment variables");
-        throw new Exception("UserServiceUrl not found in environment variables");
-    }
-    else
-    {
-        logger.Info($"UserServiceUrl: {userServiceUrl}");
-    }
-
-    builder.Services.AddHttpClient<IUserService, UserService>(client =>
-    {
-        client.BaseAddress = new Uri(userServiceUrl);
-    });
-
-    // Configure CORS
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy("AllowOrigin", builder =>
-        {
-            builder.AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-    });
+    builder.Services.AddTransient<VaultService>();
 
     // Configure JWT Authentication
     builder.Services.AddAuthentication(options =>
@@ -99,14 +65,14 @@ try
     {
         options.TokenValidationParameters = new TokenValidationParameters()
         {
-            ValidateIssuer = true, // Validate the token issuer
-            ValidateAudience = true, // Validate the token audience
-            ValidateLifetime = true, // Validate token expiry
-            ValidateIssuerSigningKey = true, // Validate signing key
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
             ValidIssuer = myIssuer,
             ValidAudience = "http://localhost",
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret)),
-            ClockSkew = TimeSpan.Zero // Disable default clock skew
+            ClockSkew = TimeSpan.Zero // remove delay of token when expire
         };
 
         options.Events = new JwtBearerEvents
@@ -116,47 +82,73 @@ try
                 if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
                 {
                     context.Response.Headers.Add("Token-Expired", "true");
-                    logger.Error($"Token expired: {context.Exception.Message}");
+                    logger.Error("Token expired: {0}", context.Exception.Message);
                 }
                 return Task.CompletedTask;
             }
         };
     });
 
-    // Configure Authorization Policies
     builder.Services.AddAuthorization(options =>
     {
-        options.AddPolicy("UserRolePolicy", policy => policy.RequireRole("1")); // Policy for users
-        options.AddPolicy("AdminRolePolicy", policy => policy.RequireRole("2")); // Policy for administrators
+        options.AddPolicy("UserRolePolicy", policy => policy.RequireRole("1"));
+        options.AddPolicy("AdminRolePolicy", policy => policy.RequireRole("2"));
     });
 
-    // Configure Logging
-    builder.Logging.ClearProviders(); // Clear default logging providers
-    builder.Host.UseNLog(); // Use NLog as the logging provider
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowOrigin", builder =>
+        {
+            builder.AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+    });
 
-    // Build and configure middleware pipeline
+    var userServiceUrl = Environment.GetEnvironmentVariable("userservicehost");
+    if (string.IsNullOrEmpty(userServiceUrl))
+    {
+        logger.Error("UserServiceUrl not found in environment variables");
+        throw new Exception("UserServiceUrl not found in environment variables");
+    }
+    else
+    {
+        logger.Info("UserServiceUrl: {0}", userServiceUrl);
+    }
+
+    builder.Services.AddHttpClient<IUserService, UserService>(client =>
+    {
+        client.BaseAddress = new Uri(userServiceUrl);
+    });
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
     var app = builder.Build();
 
     if (app.Environment.IsDevelopment())
     {
-        app.UseSwagger(); // Enable Swagger in development environment
+        app.UseSwagger();
         app.UseSwaggerUI();
     }
 
-    app.UseHttpsRedirection(); // Redirect HTTP to HTTPS
-    app.UseCors("AllowOrigin"); // Enable configured CORS policy
-    app.UseAuthentication(); // Enable JWT authentication
-    app.UseAuthorization(); // Enable role-based authorization
-    app.MapControllers(); // Map controllers to endpoints
-
-    app.Run(); // Run the application
+    app.UseHttpsRedirection();
+    app.UseCors("AllowOrigin");
+    app.UseAuthentication(); 
+    app.UseAuthorization();
+    app.MapControllers();
+    
+    app.Run();
 }
 catch (Exception ex)
 {
-    logger.Error(ex, "Stopped program because of exception"); // Log any exceptions during startup
+    logger.Error(ex, "Stopped program because of exception");
     throw;
 }
 finally
 {
-    NLog.LogManager.Shutdown(); // Properly shut down NLog
+    NLog.LogManager.Shutdown();
 }
